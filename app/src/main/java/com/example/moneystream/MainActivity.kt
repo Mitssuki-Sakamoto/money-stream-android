196e23fde4883aa8e668309a68be671dbb4749a6
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-10 18:29:31 +0900

    タブ切り替え画面

--- app/src/main/java/com/example/moneystream/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.moneystream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
     }
 }
