4e09526871090f24fe0d382086228b9221fa2994
DaigoAikawa <daikon0110@gmail.com>
2020-09-25 03:46:59 +0900

    予算履歴のフォームの概形を作成

---
+++ app/src/main/java/com/example/moneystream/EditBudgetHistory.kt
@@ -0,0 +1,11 @@
+package com.example.moneystream
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditBudgetHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_budget_history)
+    }
+}
--- app/src/main/java/com/example/moneystream/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.moneystream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txt.setOnClickListener {
+            val intent = Intent(this, EditBudgetHistory::class.java)
+            startActivity(intent)
+        }
     }
 }
