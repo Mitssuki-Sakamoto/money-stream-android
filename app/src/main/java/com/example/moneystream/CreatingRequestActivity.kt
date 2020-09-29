41c9bc89d384203ac7c6b9c097ef57280ce61af3
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-29 14:45:26 +0900

    add creatingRequest

---
+++ app/src/main/java/com/example/moneystream/CreatingRequestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moneystream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatingRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creating_request)
+    }
+}
