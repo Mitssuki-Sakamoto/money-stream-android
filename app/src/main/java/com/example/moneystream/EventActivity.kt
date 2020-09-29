11bd2ad0e07d86c8f49dc74d03186b9f72be3c18
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-29 13:24:12 +0900

    delete some files

--- app/src/main/java/com/example/moneystream/EventActivity.kt
+++ app/src/main/java/com/example/moneystream/EventActivity.kt
@@ -1,6 +1,7 @@
 package com.example.moneystream
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.moneystream.tabs.SectionsPagerAdapter
@@ -21,4 +22,5 @@ class EventActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
     }
+
 }
--- app/src/main/java/com/example/moneystream/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.moneystream
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
-import com.example.moneystream.tabs.SectionsPagerAdapter
-import com.google.android.material.tabs.TabLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event)
+        setContentView(R.layout.activity_main)
+    }
 
-        val sectionsPagerAdapter =
-            SectionsPagerAdapter(
-                this,
-                supportFragmentManager
-            )
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+    fun onClick(view: View?) {
+        val intent = Intent(this, EventActivity::class.java)
+        startActivity(intent)
     }
 }
