fe612bb3bbb4b09148e7c231b40fd9ba30368b72
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-29 12:16:43 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/moneystream/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/MainActivity.kt
@@ -9,7 +9,7 @@ import com.google.android.material.tabs.TabLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_event)
 
         val sectionsPagerAdapter =
             SectionsPagerAdapter(
