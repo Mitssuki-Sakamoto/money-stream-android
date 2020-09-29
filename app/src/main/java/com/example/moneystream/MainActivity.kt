4e5a4c50057e374c70433f6c0be9abfb9107d99f
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-29 14:04:57 +0900

    from main to event activity by click

--- app/src/main/java/com/example/moneystream/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/MainActivity.kt
@@ -10,16 +10,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = findViewById<ListView>(R.id.listViewEvent) as ListView
+        val tmpnames = arrayListOf<String>("熱海", "沖縄")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tmpnames)
+        list!!.adapter = adapter
+        list.setOnItemClickListener {parent, view, position, id ->
+            onClick(list)
+        }
     }
 
     fun onClick(view: View?) {
-        val intent = Intent(this, EventActivity::class.java)
+        val intent: Intent = Intent(this, EventActivity::class.java)
         startActivity(intent)
     }
 }
