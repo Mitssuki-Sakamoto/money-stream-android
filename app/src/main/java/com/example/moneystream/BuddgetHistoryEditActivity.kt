ead44641d0a871a1b4af336e8e1a84031aeb3261
YuukiTerui <teriyaki.tech@gmail.com>
2020-09-29 15:21:32 +0900

    add activities

---
+++ app/src/main/java/com/example/moneystream/BuddgetHistoryEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moneystream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuddgetHistoryEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buddget_history_edit)
+    }
+}
---
+++ app/src/main/java/com/example/moneystream/OwnHistoryeditActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moneystream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moneystream.R
+
+class OwnHistoryeditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_own_historyedit)
+    }
+}
