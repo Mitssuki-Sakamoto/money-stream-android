b4c690e31f20604b7cd3ac5ff5f380abcf26238d
mitsuki <mitsuki.sakamoto@uec.ac.jp>
2020-10-20 10:15:21 +0900

    パッケージフォルダーの作成

--- app/src/main/java/com/example/moneystream/presenters/activities/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/presenters/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moneystream
+package com.example.moneystream.presenters.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moneystream.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
