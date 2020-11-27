1494e2a443c18a7b9b4e592de5f0bbec46b8cbdc
YuukiTerui <teriyaki0839@gmail.com>
2020-11-27 18:47:53 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/moneystream/presenters/activities/MainActivity.kt
+++ app/src/main/java/com/example/moneystream/presenters/activities/MainActivity.kt
@@ -11,8 +11,26 @@ import android.widget.Toast
 import com.example.moneystream.R
 import com.example.moneystream.presenters.activities.BaseActivity
 import com.example.moneystream.presenters.fragments.EventListFragment
+import com.example.moneystream.presenters.fragments.EventListFragment.FragmentListener
 
-class MainActivity : BaseActivity(), EventListFragment.FragmentListener {
+class MainActivity : BaseActivity(),FragmentListener {
+    /*
+    private val eventListFragmentListener: FragmentListener = object:FragmentListener{
+        override fun onClickFab() {
+            Toast.makeText(this@MainActivity, "Clicked: fab", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, CreateEventActivity::class.java)
+            startActivity(intent)
+        }
+
+        override fun onClickListItem() {
+            Toast.makeText(this@MainActivity, "Clicked: List", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@MainActivity, EventActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
