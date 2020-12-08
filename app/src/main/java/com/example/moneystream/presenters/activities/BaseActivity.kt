90d5e7fb2f76de186ffe04f6210f3c1b2edcde2c
YuukiTerui <teriyaki0839@gmail.com>
2020-12-08 17:34:31 +0900

    Update BaseActivity.kt

--- app/src/main/java/com/example/moneystream/presenters/activities/BaseActivity.kt
+++ app/src/main/java/com/example/moneystream/presenters/activities/BaseActivity.kt
@@ -8,12 +8,4 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.moneystream.R
 import kotlinx.android.synthetic.main.my_toolbar.view.*
 
-open class BaseActivity : AppCompatActivity() {
-    var toolbar: Toolbar = findViewById(R.id.toolbar)
-    fun init_toolbar(back: Int=View.VISIBLE, title: Int = View.VISIBLE, account: Int=View.VISIBLE, detail: Int=View.VISIBLE) {
-        toolbar.image_back.visibility = back
-        toolbar.pageTitle.visibility = title
-        toolbar.image_myaccount.visibility = account
-        toolbar.image_detail.visibility = detail
-    }
-}
+open class BaseActivity : AppCompatActivity() {}
