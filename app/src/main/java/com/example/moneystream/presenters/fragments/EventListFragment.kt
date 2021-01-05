becba43713b4cf2289aa91b902dac43ab3c1ed41
YuukiTerui <teriyai0839@gmail.com>
2021-01-05 23:30:29 +0900

    fix

--- app/src/main/java/com/example/moneystream/presenters/fragments/EventListFragment.kt
+++ app/src/main/java/com/example/moneystream/presenters/fragments/EventListFragment.kt
@@ -19,11 +19,7 @@ import java.lang.RuntimeException
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EventListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class EventListFragment : BaseFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
