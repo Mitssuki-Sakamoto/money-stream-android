325673be95e168372a9a0c9c840d1c4804f5d9c2
YuukiTerui <teriyaki.tech@gmail.com>
2021-01-19 19:46:37 +0900

    Create MemberAdapter.kt

---
+++ app/src/main/java/com/example/moneystream/presenters/views/MemberAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.moneystream.presenters.views
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moneystream.R
+
+class MemberAdapter(private val context: Context?,
+                    private val itemClickListener: MemberHolder.ItemClickListener,
+                    private val itemList: List<MemberItem>)
+    :RecyclerView.Adapter<MemberHolder>() {
+
+        private var myRecyclerView: RecyclerView? = null
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        myRecyclerView = recyclerView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberHolder {
+        val layout = LayoutInflater.from(parent.context)
+        val view = layout.inflate(R.layout.recycler_item, parent, false)
+        view.setOnClickListener{
+            myRecyclerView?.let {
+                itemClickListener.onItemClick(view, it.getChildAdapterPosition(view))
+            }
+        }
+        return MemberHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MemberHolder, position: Int) {
+        holder.let {
+            var item = itemList[position]
+            it.itemImage.setImageResource(item.icon)
+            it.itemName.text = item.name
+        }
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
