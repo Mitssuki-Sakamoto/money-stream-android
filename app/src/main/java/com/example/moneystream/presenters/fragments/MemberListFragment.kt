f778c9ede3bd61b280f51756192e7fb65107bb7b
YuukiTerui <teriyaki.tech@gmail.com>
2021-01-19 20:14:09 +0900

    Update MemberListFragment.kt

--- app/src/main/java/com/example/moneystream/presenters/fragments/MemberListFragment.kt
+++ app/src/main/java/com/example/moneystream/presenters/fragments/MemberListFragment.kt
@@ -1,35 +1,74 @@
 package com.example.moneystream.presenters.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.moneystream.R
+import com.example.moneystream.presenters.MemberItems
+import com.example.moneystream.presenters.activities.EventActivity
+import com.example.moneystream.presenters.views.MemberAdapter
+import com.example.moneystream.presenters.views.MemberHolder
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
-open class MemberListFragment : BaseListFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MemberListFragment : BaseListFragment(), MemberHolder.ItemClickListener {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var myAdapter: RecyclerView.Adapter<*>
+    private lateinit var manager: RecyclerView.LayoutManager
+
+    private var listener: MemberListFragment.FragmentListener? = null
+
+    interface FragmentListener {
+        fun onClickFab()
+        fun onClickListItem()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
 
-        return inflater.inflate(R.layout.fragment_member_list, container, false)
+        val fragment = inflater.inflate(R.layout.fragment_member_list, container, false)
+        myAdapter = MemberAdapter(this.context, this, MemberItems())
+        manager = LinearLayoutManager(this.context)
+        recyclerView = fragment.findViewById<RecyclerView>(R.id.member_recycler).apply {
+            setHasFixedSize(true)
+            this.layoutManager = manager
+            this.adapter = myAdapter
+        }
+        return fragment
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is FragmentListener) {
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onItemClick(view: View, position: Int) {
+        Toast.makeText(this.context, "clicked", Toast.LENGTH_SHORT).show()
+        val intent = Intent(context, EventActivity::class.java)
+        startActivity(intent)
     }
 
     companion object {
