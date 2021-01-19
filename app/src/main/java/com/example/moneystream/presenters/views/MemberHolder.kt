7753bd5e610b02615ca19dc7093ce9dc6b414529
YuukiTerui <teriyaki.tech@gmail.com>
2021-01-19 19:40:57 +0900

    Create MemberHolder.kt

---
+++ app/src/main/java/com/example/moneystream/presenters/views/MemberHolder.kt
@@ -0,0 +1,16 @@
+package com.example.moneystream.presenters.views
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moneystream.R
+
+class MemberHolder(view: View) : RecyclerView.ViewHolder(view) {
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    var itemImage: ImageView = view.findViewById(R.id.item_image)
+    var itemName: TextView = view.findViewById(R.id.item_title)
+}
